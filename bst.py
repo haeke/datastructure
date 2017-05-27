93a52cdf82a7cb6c73f4e7d27bfbf6dc2e251ae0
Edwin Rivera <waehake@gmail.com>
2017-05-26 23:21:49 -0400

    added a binary search tree

---
+++ bst.py
@@ -0,0 +1,58 @@
+"""
+    create a binary search tree
+    this tree keeps the smallest nodes on the left
+    largest nodes to the right
+"""
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BST(object):
+    def __init__(self, root):
+        self.root = Node(root)
+
+    def insert(self, new_val):
+        self.insert_helper(self.root, new_val)
+
+    def insert_helper(self, current, new_val):
+        if current.value < new_val:
+            if current.right:
+                self.insert_helper(current.right, new_val)
+            else:
+                current.right = Node(new_val)
+        else:
+            if current.left:
+                self.insert_helper(current.left, new_val)
+            else:
+                current.left = Node(new_val)
+
+    def search(self, find_val):
+        return self.search_helper(self.root, find_val)
+
+    def search_helper(self, current, find_val):
+        if current:
+            if current.value == find_val:
+                return True
+            elif current.value < find_val:
+                return self.search_helper(current.right, find_val)
+            else:
+                return self.search_helper(current.left, find_val)
+            return False
+
+#setup the tree
+tree = BST(4)
+
+#insert elements to the tree
+tree.insert(3)
+tree.insert(4)
+tree.insert(7)
+tree.insert(12)
+
+#test searching for an item that is in the tree
+print tree.search(4)
+
+#test searching for an item that isn't in the tree
+print tree.search(13)
