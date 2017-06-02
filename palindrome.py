0ae480c63ab94ffc9275f7bcf8e838b46eb1bcb6
Edwin Rivera <waehake@gmail.com>
2017-06-01 23:30:34 -0400

    updated palindrome

---
+++ palindrome.py
@@ -0,0 +1,32 @@
+### Question 2 main function and helper functions.
+"""Given a string a, find the longest palindromic substring contained in a.
+Your function definition should look like question2(a), and return a string."""
+
+"""
+    solution is O(n^3)
+    check to see if each substring is a palindrome or not, we check all the substrings 
+
+"""
+
+def ret_substrings(s):
+    l = len(s)
+
+    for e in range(l, 0, -1):
+        for i in range(l - e + 1):
+            #return an object containing the substring from largest to shortest
+            yield s[i: i+e]
+
+# Define palindrome.
+def palindrome(s):
+    #check to see if the substring is the same as the string provided
+    return s == s[::-1]
+
+#for the string provided get all the substrings
+#compare them to the string provided then return
+def question2(a):
+    for l in ret_substrings(a):
+        if palindrome(l):
+            return l
+
+# Simple test case.
+print question2("moms")
