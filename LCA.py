a56f6c81b02412e2e86dd93b66dc98e35701b2ab
Edwin Rivera <waehake@gmail.com>
2017-06-01 23:17:15 -0400

    added an algorithm to find the lowest common ancestor from a given matrix, root, and two random nodes

--- LCA.py
+++ LCA.py
@@ -1,4 +1,22 @@
+"""
 
+Find the least common ancestor between two nodes on a binary search tree. The least common ancestor is the farthest node from the root that is an ancestor of both nodes. For example, the root is a common ancestor of all nodes on the tree, but if both nodes are descendents of the root's left child, then that left child might be the lowest common ancestor. You can assume that both nodes are in the tree, and the tree itself adheres to all BST properties. The function definition should look like question4(T, r, n1, n2), where T is the tree represented as a matrix, where the index of the list is equal to the integer stored in that node and a 1 represents a child node, r is a non-negative integer representing the root, and n1 and n2 are non-negative integers representing the two nodes in no particular order. For example, one test case might be
+
+question4([[0, 1, 0, 0, 0],
+           [0, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0],
+           [1, 0, 0, 0, 1],
+           [0, 0, 0, 0, 0]],
+          3,
+          1,
+          4)
+and the answer would be 3.
+
+"""
+
+"""
+    To complete this question I needed to create a binary search tree out of the matrix that was provided. Then I placed nodes that were provided given the rules of a binary search tree ( cannot be larger than the root, anything smaller than the current head should move to the left, anything larger than the head should move to the right).
+"""
 
 class Node:
 
@@ -28,8 +46,8 @@ class binsearchtree(object):
         self.insert_rule(self.root, new_val)
 
     # find the correct location for the new node in the tree
-    # if the current value is less than move right
-    #if the current value is more than move left
+    # if the current value is less than insert right
+    #if the current value is more than insert left
     def insert_rule(self, current, new_val):
         if current.value < new_val:
             if current.right:
